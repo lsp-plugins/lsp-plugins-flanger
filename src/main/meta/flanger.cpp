5a4463ce1c29526c8c440827816ddcad6211c116
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-02-04 23:42:24 +0300

    Using new mechanism for registering plugin metadata

--- src/main/meta/flanger.cpp
+++ src/main/meta/flanger.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2025 Linux Studio Plugins Project <https://lsp-plug.in/>
- *           (C) 2025 Vladimir Sadovnikov <sadko4u@gmail.com>
+ * Copyright (C) 2026 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2026 Vladimir Sadovnikov <sadko4u@gmail.com>
  *
  * This file is part of lsp-plugins-flanger
  * Created on: 25 нояб. 2020 г.
@@ -20,6 +20,7 @@
  */
 
 #include <lsp-plug.in/plug-fw/meta/ports.h>
+#include <lsp-plug.in/plug-fw/meta/registry.h>
 #include <lsp-plug.in/shared/meta/developers.h>
 #include <private/meta/flanger.h>
 
@@ -249,6 +250,7 @@ namespace lsp
             mono_plugin_port_groups,
             &flanger_bundle
         };
+        LSP_REGISTER_METADATA(flanger_mono);
 
         const plugin_t flanger_stereo =
         {
@@ -279,6 +281,8 @@ namespace lsp
             stereo_plugin_port_groups,
             &flanger_bundle
         };
+        LSP_REGISTER_METADATA(flanger_stereo);
+
     } /* namespace meta */
 } /* namespace lsp */
 
